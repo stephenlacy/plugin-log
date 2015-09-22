7af8c64cf5793a8a6558f82ad2b0bcacf4201dde
Steve Lacy <me@slacy.me>
2015-09-22 10:30:32 -0700

    minor cleanup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var chalk = require('chalk');
 var dateformat = require('dateformat');
 
 module.exports = function(){
-  var time = '['+chalk.grey(dateformat(new Date(), 'HH:MM:ss'))+']';
+  var time = '[' + chalk.grey(dateformat(new Date(), 'HH:MM:ss')) + ']';
   process.stdout.write(time + ' ');
   console.log.apply(console, arguments);
   return this;
